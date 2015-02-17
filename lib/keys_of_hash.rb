dafddd318cd157877a8029827a59dd3c9ce48f21
axton21 <axton_n@yahoo.com>
2015-02-17 10:01:41 -0500

    yea, yea, yea

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  require 'pry'
+
+  def keys_of(*arguments)
+    new_array = []
+    self.each do |key, value|
+      arguments.each do |country|
+        if country == value
+          new_array << key
+        # binding.pry
+        end
+      end
+    end
+      new_array
   end
 end
